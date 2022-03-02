d390ce1b16e2bb9ba6426564d60aa48468b7c3a6
NethSaranga <saranga.j@elysiancrest.com>
2022-03-02 18:47:17 +0530

    aplify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import Amplify from 'aws-amplify';
-import awsExports from './aws-exports';
-import { AlertProvider } from './app/context/alerts';
-import {AuthProvider} from './app/context/auth';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+import { AlertProvider } from "./app/context/alerts";
+import { AuthProvider } from "./app/context/auth";
+import * as serviceWorker from "./serviceWorker";
 import "../src/styles/scss/style.scss";
 
 Amplify.configure(awsExports);
@@ -23,7 +23,7 @@ ReactDOM.render(
       </AlertProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
